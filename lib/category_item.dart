ca5d2b5cf43c286ec224c177e74e39178d6a1530
Anton Smirnov <antosha-85@yandex.ru>
2021-05-16 03:12:51 -0600

    Modified m categoryMealsScreen widget to pass parameters while using Navigator

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -2,14 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:meal_app/category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
+  final String id;
   final String title;
   final Color color;
 
-  CategoryItem(this.title, this.color);
+  CategoryItem(
+    this.id,
+    this.title,
+    this.color,
+  );
 
   void selectCategory(ctx) {
     Navigator.of(ctx).push(MaterialPageRoute(builder: (_) {
-      return CategoryMealsScreen();
+      return CategoryMealsScreen(id, title);
     }));
   }
 
