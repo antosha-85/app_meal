3b4bd79f5c4f9762ba5e76efa3e7d1dc34a463ff
Anton Smirnov <antosha-85@yandex.ru>
2021-05-16 02:19:03 -0600

    Created a new theming for title and body, imported two new fonts and applied that fpr category item

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -5,15 +5,19 @@ import 'package:meal_app/dummy_data.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: DUMMY_CATEGORIES.map((catData) {
-        return CategoryItem(catData.title, catData.color);
-      }).toList(),
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20),
+    return Scaffold(
+      appBar: AppBar(title: const Text('My Meal!')),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        children: DUMMY_CATEGORIES.map((catData) {
+          return CategoryItem(catData.title, catData.color);
+        }).toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20),
+      ),
     );
   }
 }
