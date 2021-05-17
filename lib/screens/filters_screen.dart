768e66e8efab15307e6621f76313960bf553d352
Anton Smirnov <antosha-85@yandex.ru>
2021-05-17 03:13:39 -0600

    Replaced pushNamed with pushReplacementNamed to save memory

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meal_app/widgets/main_drawer.dart';
 
 class FiltersScreen extends StatelessWidget {
   static const routeName = '/filters';
@@ -8,6 +9,7 @@ class FiltersScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Filters'),
       ),
+      drawer: MainDrawer(),
       body: Center(
         child: Text('Filters'),
       ),
